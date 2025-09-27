5a7de038174e71f128e080a846e549d1ed1f5499
rlozll <ham60108@gmail.com>
2025-09-27 20:55:05 +0900

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -30,7 +30,7 @@ flask db upgrade
 
 # Start CTFd
 echo "Starting CTFd"
-exec gunicorn 'CTFd:create_app()' \
+exec /opt/venv/bin/gunicorn 'CTFd:create_app()' \
     --bind '0.0.0.0:8000' \
     --workers $WORKERS \
     --worker-tmp-dir "$WORKER_TEMP_DIR" \
